733217545e703893bbafca59e4eb386bfb3293aa
ChanYong <qkrcksdyd99@gmail.com>
2023-04-16 14:27:10 +0900

    basic file/folder setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store"; //Vuex 저장소 추가
+import mixins from "./mixins"; //믹스인 추가
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mixin(mixins).mount("#app");
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+// Vuex Store 관련 파일을 저장하는 폴더
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {},
+  mutation() {},
+  actions() {},
+  getters() {},
+});
+
+export default store;
