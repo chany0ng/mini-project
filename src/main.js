d84f7f172091cc3d82cec0afeb12d8040eb6e831
ChanYong <qkrcksdyd99@gmail.com>
2023-05-04 23:17:37 +0900

    change to script setup

--- src/main.js
+++ src/main.js
@@ -5,5 +5,4 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store"; //Vuex 저장소 추가
 import mixins from "./mixins"; //믹스인 추가
-
 createApp(App).use(store).use(router).mixin(mixins).mount("#app");
---
+++ src/views/CompositionEx.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <p>{{ person1.name }} {{ person1.value }}</p>
+    <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "@vue/composition-api";
+
+name: "composition-practice";
+const person1 = ref({ name: "sibal", age: 20 });
+const person2 = reactive({ name: "gaesae", age: 69 });
+const handleClick = () => {
+  person1.value.age = 30;
+  person2.age = 30;
+};
+</script>
+
+<style></style>
